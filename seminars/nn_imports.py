b22f9d1402c38e18f33a7ecafb7a9530fc87b998
Цвигун Аким Олегович <AOTsvigun@sberbank.ru>
2021-10-03 18:19:11 +0300

    Seminars 1-4 & assignment 1 added

---
+++ seminars/nn_imports.py
@@ -0,0 +1,13 @@
+
+from IPython.display import display, clear_output
+
+import numpy as np
+import pandas as pd
+from tqdm.notebook import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset, DataLoader
